7db540125526c65d8d3764cbf106d429c01c8a15
kodenart <mjuanvitorp@gmail.com>
2022-02-04 14:45:44 -0300

    iniciando o projeto =)

---
+++ src/components/Form.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Form extends Component {
+  render() {
+    return (
+      <form>
+        <h1>just to test</h1>
+      </form>
+    );
+  }
+}
